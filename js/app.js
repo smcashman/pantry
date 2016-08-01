e0aaeb79cf8c19557ef272964d39d6150d6766aa
Sara Cashman <saramcashman@gmail.com>
2016-08-01 18:36:11 -0400

    final jquery

--- js/app.js
+++ js/app.js
@@ -1,30 +1,47 @@
 
 
 $(document).ready(function(){
-$("button#add").click(function(){
+
+	$('input#item').on('keydown', function(e) {
+ 	if (e.which == 13) {
+ 	if ($("input#item").val()){
+
+       $("button#add").click();
+ 	var itemToAdd = $("input#item").val();
+ }
+ 	};
+ 	    
+	$("button#add").click(function(){
 
 	if ($("input#item").val()){
 	var itemToAdd = $("input#item").val();
-	$("ul.toget").append("<li><i id='uncheck' class='fa fa-square-o'></i> "+itemToAdd+" <i id='trash' class='fa fa-trash-o'></i></li>");
+	$("ul.toget").append("<li class='added'><i id='uncheck' class='fa fa-square-o'></i><span> "+itemToAdd+" </span><i id='trash' class='fa fa-trash-o'></i></li>");
+	};
 	$("input#item").val("").focus();
-	}
-
-	$(".toget li").click(function() {
-	$(this).toggleClass("clicked");
-	});
-
-	$("#trash").click(function() {
-	$(".toget li").remove();
-
-	})
-
-	
-	
-
-
-	
-
- });
-
+	  
 
 });
+	
+ 		});
+	$("ul.toget").off().click(function() {
+
+
+	$('ul.toget').delegate('li','click',function() {
+		console.log("CLICKED");
+ 		$(this).toggleClass('clicked');
+ 		if ($(this).children('i#uncheck').hasClass('fa-square-o')) {
+ 			$(this).children('i#uncheck').removeClass('fa-square-o');
+ 			$(this).children('i#uncheck').addClass('fa-check-square-o');
+ 		} else if ($(this).children('i#uncheck').hasClass('fa-check-square-o')) {
+ 			$(this).children('i#uncheck').addClass('fa-square-o');
+ 			$(this).children('i#uncheck').removeClass('fa-check-square-o');
+ 		};
+ 	});
+
+})
+
+	$('ul.toget').delegate('li i#trash','click',function() {
+ 	$(this).parent("li").remove();
+  	});		  	
+});
+//});
